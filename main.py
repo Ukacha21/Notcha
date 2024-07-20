9f9e03026d1cf42c15b3a362f49a74d2851721ae
Ukacha Juma <70150736+Ukacha21@users.noreply.github.com>
2024-07-20 22:08:07 +0300

    Update main.py
    Updates in main.py [
    
    transparent color to #050513 (something close to black)
    geometry height in hidden mode to 1 px
    ]

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ button = #438499
 root = tk.Tk()
 bgcolor = "#121016"#"#004538"
 fgcolor = "white"
-root['bg'] = "blue" #transparentcolor reference
+root['bg'] = "#050513" #transparentcolor reference
 root.overrideredirect(1)
 
 #root.title("Centered!") 
 root.attributes("-topmost", True)
-root.attributes("-transparentcolor", "blue")
+root.attributes("-transparentcolor", "#050513")
 # root.eval('tk::PlaceWindow . top')
 #root.eval('tk::PlaceWindow . top')
 
@@ -26,7 +26,7 @@ state = False
 barimg = tk.PhotoImage(file="bg7.png")
 topmost_button_image = tk.PhotoImage(file="button3.png")
 
-image_label = tk.Label(root, image = barimg, bg='blue')
+image_label = tk.Label(root, image = barimg, bg='#050513')
 image_label.pack()
 
 time_label = tk.Label(root, bg=bgcolor, foreground=fgcolor)
@@ -41,16 +41,14 @@ top_enabled = True
 def top_enable_func(*args):
     global top_enabled
     # top_enabled = True if not top_enabled else False
-
     top_enabled = False
     show_hide()
     
 def show_hide(*args):
     global top_enabled
     if top_enabled != True: #hide
-
         # root.attributes("-topmost", top)
-        root.geometry(f"{414}x{5}+{x}+{0}") 
+        root.geometry(f"{414}x{1}+{x}+{0}") #set height(currently 1) preferrably between 1-5
         top_enabled = True
         root.bind("<Enter>", show_hide)
         print("no longer on top")
@@ -70,7 +68,6 @@ def update(label):
     label.place_configure(y = 0, x = (root.winfo_reqwidth()//2 - label.winfo_reqwidth()//2))
     # print(label.winfo_reqwidth())
     Timer(1, lambda: update(label)).start()
-
 update(time_label)
 
 def center_window_bottom(win):
@@ -82,14 +79,11 @@ def center_window_bottom(win):
     window_width = win.winfo_reqwidth()
 
     global x
-
     y = screen_height - window_height #places o=to the bottom on y vertical axis
     x = screen_width//2 - window_width//2 #winwow srarts from the exact center, does not exactly place it to the center
 
-
     #win.geometry(f"{screen_width//4}x{screen_height//110}+{x}+{0}") # f"{new_width}x{new_height}+{x_pos}+{y_pos}"
     win.geometry(f"{414}x{34}+{x}+{0}") # f"{new_width}x{new_height}+{x_pos}+{y_pos}"
-
 center_window_bottom(root)  # Call the centering function before packing widgets
 
 root.mainloop()
